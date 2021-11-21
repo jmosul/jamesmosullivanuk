03b36da2e131ffb450bd7cae648b4ce19a726353
James <jmosul@users.noreply.github.com>
2021-11-21 14:45:33 +0000

    fix: missing p tag

--- src/pages/Critmas/pages/CritmasElves/componets/ElfSpy.tsx
+++ src/pages/Critmas/pages/CritmasElves/componets/ElfSpy.tsx
@@ -15,7 +15,7 @@ function ElfSpy() {
                     Have you seen how many children there are? Billions.  And Santa needs to keep track of all them.  Which of them are naughty and which of them are nice.  And also, just what sort of toys are they into these days?
                 </p>
               
-                <p>Santa's agents are placed everywhere from large department stores to posts in child care. They work hard to keep up appearences, but they're also reporting back to North Pole HQ.
+                <p>Santa's agents are placed everywhere from large department stores to posts in child care. They work hard to keep up appearances, but they're also reporting back to North Pole HQ.</p>
 
                 <p>Has the world changed you? Is it too big?  Or is it wonderful to be out there?  And, just why are you back at the North Pole and not at your post?</p>
             </Card.Body>
